9fec77ecddafd6523f75e14d74ae969746b4a401
awilson12 <apfeifer@email.arizona.edu>
2020-05-19 14:25:41 -0700

    start of files and repository

---
+++ QMRA_code_automated.R
@@ -0,0 +1,6 @@
+#Developed by Amanda M. Wilson and Mark H. Weir
+
+#Bleach QMRA Code - relating bleach efficacies to estimated COVID-19 infection risks
+
+#Exposure scenario: single hand-to-surface contact, followed by single hand-to-mouth contact (dose)
+
